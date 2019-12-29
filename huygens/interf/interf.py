ac5f324af56422383da6f603c3506f3960f66c1d
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2019-12-30 10:47:31 +1300

    Added runtime checks to c_vector

--- huygens/interf/interf.py
+++ huygens/interf/interf.py
@@ -1,8 +1,21 @@
 
 import ctypes as ct
+import _ctypes as _ct
 
 def c_vector(dtype,size):
-  return (dtype*size)()
+
+  try:
+    if _ct._SimpleCData not in dtype.__mro__:
+      raise TypeError('`dtype` must be a ctypes data type.')
+  except AttributeError:
+    raise TypeError('`dtype` must be a ctypes data type.')
+
+  try:
+    return (dtype*size)()
+  except TypeError:
+    raise TypeError('`size` must be a non-negative integer.')
+  except ValueError:
+    raise ValueError('`size` must be a non-negative integer')
 
 def c_matrix(dtype,nrow,ncol):
   tmp=_c_2d_tmp(dtype,nrow)
