966ac9cd34beff02784c78f264f13681a388024a
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-02 13:24:14 +1300

    Merge branch dev

--- setup.py
+++ setup.py
@@ -7,13 +7,19 @@ D. C. Groothuizen Dijkema - December, 2019
 
 import setuptools
 
+major=0
+minor=2
+micro=1
+
+version='{}.{}.{}'.format(major,minor,micro)
+
 setuptools.setup(
   author='D. C. Groothuizen Dijkema',
   author_email='',
   name='huygens',
   license='MIT',
   description='huygens is my personal Python library of commonly used functions.',
-  version='0.2.0',
+  version=version,
   long_description=open('README.md').read(),
   url='https://github.com/DCGroothuizenDijkema/huygens',
   packages=setuptools.find_packages(),
