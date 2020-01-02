2c81ba716d7a0b4c1be977880ae12470b4abc9b9
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-02 13:17:51 +1300

    Expanded docstrings; added comments; reordered components

--- huygens/plotting/plotting.py
+++ huygens/plotting/plotting.py
@@ -21,8 +21,12 @@ def simulation_plot(data=None,x_axis=None,hline=None,ax=None,fig=None,x_lim=None
   x_axis : array_like (n,), optional
     The values of the independent variable, to plot the data of each run against.
     This data is the x-axis value of the simulation.
+  hline : float
+    The y-axis value at which to plot a horizontal line.
   ax : matplotlib.axes.Axis, optional
-    The axis to plot on, if already configured
+    The axis to plot on, if already configured.
+  fig : matplotlib.figure.Figure, optional
+    The figure to plot on, if already configured.
   x_lim : 2-tuple of floats, optional
     The left and right limit of the x-axis of the plot.
     These limits do not have to equal the smallest and largest value in x_axis.
@@ -42,8 +46,12 @@ def simulation_plot(data=None,x_axis=None,hline=None,ax=None,fig=None,x_lim=None
     The scaling of the x-axis.
   linewidth : float, optional
     The linewith of each run's line.
+  hline_linewidth : float, optional
+    The linewith of the horizontal line.
   alpha : float, optional
     The alpha of each run's line.
+  hline_alpha : float, optional
+    The alpha of the horizontal line.
 
   Returns
   -------
@@ -54,31 +62,22 @@ def simulation_plot(data=None,x_axis=None,hline=None,ax=None,fig=None,x_lim=None
 
   '''
   with _SimulationPlotStyle():
+    # generate an axis if not passed in
     if ax is None:
-      print(ax)
-      print('Generating new axis')
       fig,ax=_setup_simulation_plot(x_lim=x_lim,y_lim=y_lim,y_axis_ticks=y_axis_ticks,title=title,x_label=x_label,y_label=y_label,
         fig_size=fig_size,scale=scale)
+    # plot the data
     for datum in data:
       ax.plot(x_axis,datum,color="#1f77b4",linewidth=linewidth,alpha=alpha)
+    # plot the horizontal line
     if hline is not None:
       ax.axhline(hline,color="#d62728",linewidth=hline_linewidth,alpha=hline_alpha)
 
+  # if we have a figure as well as an axis
   if fig is not None:
     return fig,ax
   else:
     return ax
-
-class _SimulationPlotStyle(matplotlib.rc_context):
-  '''
-  Style a simulation plot without affecting the global plotting settings
-
-  Plots are styled with the seaborn muted palette.
-  '''
-  def __init__(self, **kwargs):
-    matplotlib.rc_context.__init__(self)
-    sns.set_palette("muted",color_codes=True)
-    sns.set()
 
 def setup_simulation_plot(x_lim=None,y_lim=None,y_axis_ticks=None,title='',x_label='',y_label='',fig_size=(20,6),scale='log'):
   '''
@@ -118,6 +117,17 @@ def setup_simulation_plot(x_lim=None,y_lim=None,y_axis_ticks=None,title='',x_lab
 
   return fig,ax
 
+class _SimulationPlotStyle(matplotlib.rc_context):
+  '''
+  Style a simulation plot without affecting the global plotting settings
+
+  Plots are styled with the seaborn muted palette.
+  '''
+  def __init__(self, **kwargs):
+    matplotlib.rc_context.__init__(self)
+    sns.set_palette("muted",color_codes=True)
+    sns.set()
+
 def _setup_simulation_plot(x_lim=None,y_lim=None,y_axis_ticks=None,title='',x_label='',y_label='',fig_size=(20,6),scale='log'):
   '''
   Produce the base figure and axis for a simulation plot
@@ -152,22 +162,29 @@ def _setup_simulation_plot(x_lim=None,y_lim=None,y_axis_ticks=None,title='',x_la
   '''
   fig,ax=plt.subplots(figsize=fig_size)
   ax.set_xscale(scale)
+
+  # turn off vertical grid lines, style horizontal grid lines
   ax.grid(False,'both','x')
   ax.grid(True,'major','y',color='k',linestyle='-',linewidth=0.8,alpha=0.3)
   
+  # set the limits
   if x_lim is not None: ax.set_xlim(x_lim[0],x_lim[1])
   if y_lim is not None: ax.set_ylim(y_lim[0],y_lim[1])
 
+  # set the ticks
   if y_axis_ticks is not None: ax.set_yticks(np.arange(*y_axis_ticks))
 
+  # set the style of the borders
   for side in ['left','bottom','right','top']:
     ax.spines[side].set_linewidth(0.7)
     ax.spines[side].set_color('#333333')
 
+  # style ticks
   ax.tick_params(axis="x",which="both",top=False,bottom=True,width=0.7,labelsize=14)
   ax.tick_params(axis="y",which="both",left=True,direction="inout",width=0.7,labelsize=14)
   ax.tick_params(axis="y",which="both",right=True,direction="in",width=0.7,labelsize=14)
 
+  # set titles and labels
   ax.set_xlabel(x_label,fontsize=16)
   ax.set_ylabel(y_label,fontsize=16)
   ax.set_title(title,fontsize=18)
